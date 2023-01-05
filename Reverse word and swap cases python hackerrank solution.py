8a4479a0fb5c5332d4b1e7f83166aaa60294c4ba
Rahul kumar <119062308+rahulkumar7759@users.noreply.github.com>
2023-01-05 09:34:56 +0530

    Create Reverse word and swap cases python hackerrank solution.py

---
+++ Reverse word and swap cases python hackerrank solution.py
@@ -0,0 +1,29 @@
+
+##(Q)  1  reverse word and swap cases
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def reverse_words_order_and_swap_cases(sentence):
+    word_list = sentence.split()
+    reversed_list = word_list[:: -1]
+    reversed_sentence = " ".join(reversed_list)
+    return reversed_sentence.swapcase()
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    sentence = raw_input()
+
+    result = reverse_words_order_and_swap_cases(sentence)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
+    
+    
